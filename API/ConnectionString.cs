5c8100a52846ad05ef7e3e12ee8dc3564d8fec94
lmthompson6 <98614908+lmthompson6@users.noreply.github.com>
2022-10-20 16:07:47 -0500

    New Commit

--- API/ConnectionString.cs
+++ API/ConnectionString.cs
@@ -2,6 +2,10 @@ namespace API
 {
     public class ConnectionString
     {
-        
+        public string cs {get;}
+
+        public ConnectionString(){
+            cs = "server=qvti2nukhfiig51b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;user=nduf6o0t67b9xzc7;database=qsvus5nqiav2pm72;port=3306;password=uam7fs2odcfworwr";
+        }
     }
 }
